bf8e968f67369e2837fb8955a94026de9ac8c723
Torham <torhamdev@gmail.com>
2025-04-15 13:39:40 +0330

    ✨ Add !check command to analyze a sentence without editing it

--- main.py
+++ main.py
@@ -16,6 +16,26 @@ api_hash = os.getenv("API_HASH")
 client = TelegramClient(os.getenv("CLIENT_NAME"), api_id, api_hash)
 
 
+@client.on(events.NewMessage(outgoing=True, pattern=r"(?i).*!check"))
+async def check(event):
+    if event.is_reply:
+        new = await event.get_reply_message()
+        message = str()
+        uncorrect_words = {}
+        uncorrect_count = 0
+        for word in new.raw_text.split():
+            checked = spell_check(word, words)
+            if checked[0][1] > 0:
+                uncorrect_count += 1
+                uncorrect_words[word] = checked[0][0]
+
+        message += f"There are {uncorrect_count} posibble uncorrect words exists.\n"
+        for uncorrect, correct in uncorrect_words.items():
+            message += f"{uncorrect} -> {correct}\n"
+
+        await event.reply(message)
+
+
 @client.on(events.NewMessage(outgoing=True, pattern=r"(?i).*!fix"))
 async def read(event):
     if event.is_reply:
